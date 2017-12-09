462311b288ad35d6843baa51a124ee202510323a
Jacqueline Askins <askinsj@sas.upenn.edu>
2017-12-09 17:55:37 -0500

    Add businesses SQL query

--- routes/business.js
+++ routes/business.js
@@ -1,38 +1,30 @@
 var express = require('express');
 var router = express.Router();
-// var userPostsDb = require('../db/userPosts');
 var usersDb = require('../db/users');
 var checkValidUser = require('../middlewares/checkValidUser');
+var oracledbModule = require('../modules/oracledbModule');
 
 router.get('/business/:id/:personName/:city', function (req, res, next) {
+  renderBusinesses(req, res, next, 'SELECT name, address, stars, review_count FROM businesses WHERE city = :city');
+});
 
-  usersDb.findUser(req.session.email, function(err, user) {
-
-    res.render('business', {
-      errorMessage: req.session.errorMessage,
-      personName: req.params.personName,
-      userName: req.session.personName,
-      //personEmailName: req.params.email,
-      //id: req.params.email
-      city: req.params.city,
-      id: req.session.id
-      // ,
-      // myPosts: req.session.myPosts,
-      // isFriends: req.session.isFriends,
-      // email: req.session.email,
-      // homePosts: homePosts
-    });
-
+function renderBusinesses(req, res, next, query) {
+  usersDb.findUser(req.session.email, function (err, user) {
+    oracledbModule.handleDatabaseConnection(
+      'SELECT name, address, stars, review_count FROM businesses WHERE city = :city',
+      [req.params.city],
+      function (result) {
+        res.render('business', {
+          errorMessage: req.session.errorMessage,
+          personName: req.params.personName,
+          userName: req.session.personName,
+          businesses: result.rows,
+          city: req.params.city,
+          id: req.session.id
+        });
+      }
+    );
   });
-
-
-
-});
-
-router.post('/business/:id/:personName/:city', function (req, res, next) {
-
-
-});
-
+}
 
 module.exports = router;

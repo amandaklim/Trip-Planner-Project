93bd778a4eb418a8232b512c2bb4adfb8a82e615
Jacqueline Askins <askinsj@sas.upenn.edu>
2017-12-09 20:22:39 -0500

    Add business sorting

--- routes/business.js
+++ routes/business.js
@@ -5,14 +5,44 @@ var checkValidUser = require('../middlewares/checkValidUser');
 
 var oracledbModule = require('../modules/oracledbModule');
 
-router.get('/business/:id/:personName/:city', function (req, res, next) {
-  renderBusinesses(req, res, next, 'SELECT name, address, stars, review_count FROM businesses WHERE city = :city');
+router.get('/business/:personName/:city', function (req, res, next) {
+  res.redirect('/business/' + req.params.personName + '/' + req.params.city + '/all');
+});
+
+router.get('/business/:personName/:city/:businessType', function (req, res, next) {
+  query = getQueryForBusinessType(req.params.businessType);
+  renderBusinesses(req, res, next, query);
+});
+
+router.get('/business/:personName/:city/:businessType/sort/:sortCol/:sortOrder', function(req, res, next) {
+  query = getQueryForBusinessType(req.params.businessType) + ' ORDER BY ';
+  switch(req.params.sortCol) {
+    case 'rating':
+      query += 'stars ';
+      break;
+    case 'review_count':
+      query += 'review_count ';
+      break;
+    default:
+      query += 'name ';
+      break;
+  }
+  query += (['ASC', 'DESC'].indexOf(req.params.sortOrder) != -1 ? req.params.sortOrder : 'ASC');
+
+  oracledbModule.handleDatabaseConnection(
+    query,
+    [req.params.city],
+    function (result) {
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ businesses: result.rows });
+    }
+  );
 });
 
 function renderBusinesses(req, res, next, query) {
   usersDb.findUser(req.session.email, function (err, user) {
     oracledbModule.handleDatabaseConnection(
-      'SELECT name, address, stars, review_count FROM businesses WHERE city = :city',
+      query,
       [req.params.city],
       function (result) {
         res.render('business', {
@@ -21,11 +51,25 @@ function renderBusinesses(req, res, next, query) {
           userName: req.session.personName,
           businesses: result.rows,
           city: req.params.city,
-          id: req.session.id
+          id: req.session.id,
+          businessType: req.params.businessType
         });
       }
     );
   });
+}
+
+function getQueryForBusinessType(businessType) {
+  switch (businessType) {
+    case 'vegetarian':
+      return "SELECT name, address, stars, review_count FROM businesses WHERE city = :city AND vegetarian = 'TRUE'"
+    case 'vegan':
+      return "SELECT name, address, stars, review_count FROM businesses WHERE city = :city AND vegan = 'TRUE'"
+    case 'kosher':
+      return "SELECT name, address, stars, review_count FROM businesses WHERE city = :city AND kosher = 'TRUE'"
+    default:
+      return 'SELECT name, address, stars, review_count FROM businesses WHERE city = :city';
+  }
 }
 
 module.exports = router;
